b94aaa17a4044ca597c2df1462fadfb3ca2d8de8
David <dfletch1@binghamton.edu>
2020-04-19 19:41:29 -0400

    base sort file

---
+++ sort.h
@@ -0,0 +1,27 @@
+#ifndef SORT_H
+#define SORT_H
+#include <iostream>
+
+
+class Sorter{
+	public:
+		int * array;
+		void insertionSort(int *);
+		void mergeSort(int *);
+		void quickSort(int *, bool);
+};
+
+void Sorter::insertionSort(int * array){
+	//TODO: do
+}
+
+void Sorter::mergeSort(int * array){
+	//TODO: do
+}
+
+void Sorter::quickSort(int * array, bool random){
+	//TODO: do
+}
+
+
+#endif
